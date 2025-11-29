669d5dc3db14ad6a301cddbf117cb1613bcd8036
Sarah M <stmoham1@ualberta.ca>
2025-11-29 00:52:02 -0700

    Update mainmenu.gd

--- new-game-project/mainmenu.gd
+++ new-game-project/mainmenu.gd
@@ -9,7 +9,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().root.set_meta("story_path", "res://story/story.json")
-	get_tree().change_scene_to_file("res://vntemplate.tscn")  # load your VN scene
+	get_tree().change_scene_to_file("res://vntemplate.tscn") 
 	# store which story JSON to use
 	
 
